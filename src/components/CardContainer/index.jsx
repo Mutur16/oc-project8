177df1cc6b1bdd93dd8811a8c972c25d12369d73
Mutur <komorebi.amm@gmail.com>
2023-11-24 17:36:22 +0100

    card review

--- src/components/CardContainer/index.jsx
+++ src/components/CardContainer/index.jsx
@@ -5,21 +5,11 @@ import housingData from '../../housing.json'
 import './container.scss'
 
 function CardContainer() {
-    const firstRow = housingData.slice(0, 3)
-    const secondRow = housingData.slice(3, 6)
-
     return (
-        <div className="cardsContainer">
-            <div className="card-row">
-                {firstRow.map((data) => (
-                    <Card key={data.id} title={data.title} cover={data.cover} />
-                ))}
-            </div>
-            <div className="card-row">
-                {secondRow.map((data) => (
-                    <Card key={data.id} title={data.title} cover={data.cover} />
-                ))}
-            </div>
+        <div className="cardContainer">
+            {housingData.map((data) => (
+                <Card key={data.id} title={data.title} cover={data.cover} />
+            ))}
         </div>
     )
 }
